89173ee5f004e385371cf4896604e3724a2cc7b4
mumtaz1000 <shereenfatima1000@gmail.com>
2022-11-14 12:54:41 +0100

    C14: Sending email is almost complete

--- src/services/SendingEmail.tsx
+++ src/services/SendingEmail.tsx
@@ -1,32 +1,44 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FormEvent, useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 import { send } from 'process';
 
 const SendingEmail = () => {
   const form: any = useRef(null);
+  const current = new Date();
+
+  const templateParams: any = {
+    name: 'Mumtaz',
+    notes: 'Check this out!',
+    email: 'shereenfatima1000@gmail.com'
+};
+
+  const currentDate = `${current.getDate()}${current.getMonth()+1}${current.getFullYear()}`;
   const SERVICE_ID = "service_oyhwjo6";
   const TEMPLATE_ID = "template_cef8s9l";
   const PUBLIC_KEY = "jxBw8W1xL1Z8Z21Ar";
 
 
-  const sendEmail = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+  const sendEmail = () => {
+    emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams , PUBLIC_KEY)
       .then((result: any) => {
-          console.log(result.text);
+          console.log("Success!", result.text);
       }, (error: any) => {
           console.log(error.text);
       });
 
   };
+  /* 
+  useEffect(() => {
+    if(currentDate === "14112022")
+    {
+      setValue("mumtaz.fatima1000@gmail.com");
+      sendEmail();
+    }
+  },[])
+  */
   return (
     <div>
-      <form ref={form}
-      onSubmit={sendEmail}>
-        <label>Email</label>
-      <input type="email" name="user_email" />
-        <input type="submit" value="Send" />
+      <form ref={form} onSubmit={sendEmail}>
       </form>
     </div>
   )
